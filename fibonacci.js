2f9b0049d3db4eec99dc74c75f9aee175f7b1a3f
Hema Sirisha <85925736+hemasirisha18@users.noreply.github.com>
2022-06-03 18:00:58 +0530

    Add files via upload

--- fibonacci.js
+++ fibonacci.js
@@ -1,3 +1,26 @@
+<<<<<<< HEAD
+const { request, response } = require('express');
+const cors = require('cors');
+const express = require('express');
+const app = express();
+const port= 8080 ;
+
+app.use(cors({
+    'origin': 'http://localhost:8080'
+}));
+app.listen(port, () => {console.log("Going live and server started...") });
+app.get('/fibonacci',(request, response)=>{
+    let fib = function(number)
+    {
+        let result =0;
+        if (n<=2){ return n-1;}
+        result = fib(number-1) + fib(number-2);
+        return result;
+    };
+    response.json({'fibonacci': result});
+
+});
+=======
 const { request, response } = require('express');
 const cors = require('cors');
 const express = require('express');
@@ -19,3 +42,4 @@ app.get('/fibonacci',(request, response)=>{
     response.json({'fibonacci': result});
 
 })
+>>>>>>> acdfa4dce7da2e964e4710c729ae8ce55b337397
